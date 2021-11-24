4f92c4c8c2f26e5c274ff4cb55fad94d2e2fa1bc
Jóhan H Dam-Davidsen <johan.davidsen@fjakkarin.com>
2021-11-24 09:11:09 -0300

    Implemented Checkout

---
+++ public/checkout.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file renders the default view and loads the dependencies.
+ *
+ * @package Fjakkarin/NetsSample
+ */
+
+namespace Fjakkarin\NetsSample;
+
+use Dotenv\Dotenv;
+use Fjakkarin\NetsSample\Inc\Template;
+
+require_once __DIR__ . './../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . './../');
+$dotenv->load();
+
+Template::view('../views/checkout.html');
